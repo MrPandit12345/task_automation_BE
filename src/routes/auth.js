6d6bb51d0513e96658c50add3be3cfec577ea674
MrPandit12345 <117350192+MrPandit12345@users.noreply.github.com>
2025-12-18 02:31:10 +0530

    fixed route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/auth.controller.js";
+import { login, register } from "../controller/auth.controller";
 
 const router = express.Router();
 
